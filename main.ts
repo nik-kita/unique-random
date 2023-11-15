cd0efb7af8b9b4204041766943b662a3be94d473
nikita <nik.moiseienko@gmail.com>
2023-11-15 02:37:11 +0100

    init

--- main.ts
+++ main.ts
@@ -1,20 +1,12 @@
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { Context, Hono } from "hono/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+app.get(
+  "/",
+  (c: Context) => c.render(`<h1>// TODO add documentation</h1></hr>`),
+);
 
-app.get("/api/", (c) => c.json(data));
-
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text("No dinosaurs found.");
-  }
-});
-
-Deno.serve(app.fetch);
+Deno.serve({
+  port: 3000,
+}, app.fetch);
